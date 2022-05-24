110c96fa616f740c0e1c90703605efa85963295a
Anderson Santos <adnnskt@gmail.com>
2022-05-23 21:12:33 -0300

    add style load heros button
    Some style for button that load data of heros

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -20,7 +20,7 @@ export default props => {
                 <div className='first-card'>
                     <h2>{hero.name}</h2>
                 </div>
-                <div><button onClick={changeData}>Loading Heros</button></div>
+                <div><button onClick={changeData} className='main-button'>Loading Heros</button></div>
                 <div className='secound-card'>
                     <h2>Card 2</h2>
                 </div>
