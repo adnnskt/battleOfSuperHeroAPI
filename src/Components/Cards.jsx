01af1a7bc29ad0a44e7593e76c2536dc7fb7e499
Anderson Santos <adnnskt@gmail.com>
2022-06-18 13:11:41 -0300

    change data source
    add locar data of heros. SuperheroAPI is now off

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import '../App.css'
 import api from '../Middleware/apiData'
+import data from '../Middleware/data'
 export default props => {
     //let hero = {}  
 
@@ -10,24 +11,33 @@ export default props => {
 
  function changeData(){
     let heroData = {}
+    heroData.name = data[0].name
+    heroData.fullName = data[0].biography.aliases
+    heroData.publisher = data[0].biography.publisher
+    heroData.firstAppearance = `First appearance at ${data[0].biography['first-appearance']}`
+    heroData.connection = `Connection with ${data[0].connections['group-affiliation']}`
+    setHeroImg(<img src={data[0].images.lg} className= 'img' width= '130' height= '120'/>)
+    setHero(heroData)        
+    
+    /*
     api.get('3')
     .then(function(response){
         
         heroData.name = response.data.name
-        heroData.fullName = response.data.biography.aliases
-        heroData.publisher = response.data.biography.publisher
-        heroData.firstAppearance = `First appearance at ${response.data.biography['first-appearance']}`
-        heroData.connection = `Connection with ${response.data.connections['group-affiliation']}`
+        //heroData.fullName = response.data.biography.aliases
+        //heroData.publisher = response.data.biography.publisher
+        //heroData.firstAppearance = `First appearance at ${response.data.biography['first-appearance']}`
+        //heroData.connection = `Connection with ${response.data.connections['group-affiliation']}`
         console.log(response.data)
-        setHeroImg(<img src={response.data.image.url} className= 'img' width= '150' height= '120'/>)
-        setHero(heroData)        
+       // setHeroImg(<img src={response.data.image.url} className= 'img' width= '150' height= '120'/>)
+       // setHero(heroData)        
     })
- }
- 
+    */
+}
     return(
-        <>
         
-        <div className='container'>
+    <>
+    <div className='container'>
             <div className='main'>
                 <div className='first-card'>
                 <div className='center-img'>{heroImg}</div>                
@@ -47,6 +57,7 @@ export default props => {
             </div>
 
         </div>
+
         </>
     )
 }
